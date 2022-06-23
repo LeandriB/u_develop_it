65c1c534ee392a4300bed4e5e924d03a4e4467c7
Leandri Beavis <leandrikuyk@gmail.com>
2022-06-22 22:13:20 -0400

    Created databases

---
+++ db/schema.sql
@@ -0,0 +1,7 @@
+-- creates a table for candidates
+CREATE TABLE candidates (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    industry_connected BOOLEAN NOT NULL
+);
